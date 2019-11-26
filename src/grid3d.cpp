74ae4916a5302408dce455e41c29dc7a07c70085
hyperzy <hyperzy.yang@gmail.com>
2019-11-25 20:44:06 -0500

    Fixed a small but important bug in Marching function. Changed line 207.

--- src/grid3d.cpp
+++ src/grid3d.cpp
@@ -204,8 +204,8 @@ void Grid3d::Marching(std::priority_queue<PointKeyVal> &close_set, bool inside)
                 if (this->grid_prop[idx_ijk].fmm_status != FMM_Status::CLOSE) {
                     close_set.emplace(PointKeyVal{i, j, k, this->phi[idx_ijk]});
                     this->grid_prop[idx_ijk].fmm_status = FMM_Status::CLOSE;
+                    this->grid_prop[idx_ijk].extension_status = ExtensionStatus::EXTENSION;
                 }
-                this->grid_prop[idx_ijk].extension_status = ExtensionStatus::NATURAL;
             }
         }
         assert(isValidRange(point.i, point.j, point.k));
